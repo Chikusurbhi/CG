71be1aaa5aedca4c1f76f779afccfd358a909bd3
Chikusurbhi <35466627+Chikusurbhi@users.noreply.github.com>
2019-02-04 17:23:14 +0530

    Lab2ui

---
+++ Mpersonproject.java
@@ -0,0 +1,16 @@
+package com.cg.Lab2project.ui;
+
+import com.cg.Lab2.bean.Mperson;
+
+public class Mpersonproject {
+
+	public static void main(String[] args) {
+		
+		Mperson p= new Mperson("Aditi","Priya",'f');
+		p.getnumber(1234506789);
+		p.display();
+		
+
+	}
+
+}
---
+++ PersonMain.java
@@ -0,0 +1,24 @@
+package com.cg.Lab2project.ui; 
+
+import com.cg.Lab2.bean.Person1;
+
+public class PersonMain {
+
+	public static void main(String[] args) {
+	
+		Person1 person2= new Person1("Rohan","Mehra",'M');
+		
+		/*person2.setFirstName("Aditi");
+		person2.setLastName("Priya");
+		person2.setGender('f');
+		
+		System.out.println("Person's first name is:" + person2.getFirstName());
+		System.out.println("Person's last name is:" + person2.getLastName());
+		System.out.println("Person's gender is:" + person2.getGender());
+		*/
+		System.out.println(person2);
+	}
+
+	
+
+} 
