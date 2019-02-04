5e9369edacebb592f8b87f906cb99d7ebcecd18c
Chikusurbhi <35466627+Chikusurbhi@users.noreply.github.com>
2019-02-04 17:16:36 +0530

    Lab1

---
+++ Student.java
@@ -0,0 +1,12 @@
+package com.cg.Lab1.bean;
+
+public class Student {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("First Name:" + args[0]);
+	}
+
+}
